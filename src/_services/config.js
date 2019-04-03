05cf7b15f7f8d3ed1fe07abfb149564e4c96130c
Ocheben <ocheben@gmail.com>
2019-04-03 14:28:47 +0100

    Changed Api URL

--- src/_services/config.js
+++ src/_services/config.js
@@ -1,6 +1,5 @@
 export const URLS = {
-    LOGIN: 'http://127.0.0.1:5000/auth',
-    // LOGIN: 'https://ce-test-server.herokuapp.com/auth',
+    LOGIN: 'https://ce-test-server.herokuapp.com/auth',
 }
 
 export const METHODS = {
