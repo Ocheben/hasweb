8783bf376f22001d25ead61c3171982239ded384
Oche Onoja <oche@Oches-MacBook-Pro.local>
2020-02-04 13:24:40 +0100

    Changed url

--- src/_services/config.js
+++ src/_services/config.js
@@ -4,15 +4,15 @@ export const URLS = {
   // LOGIN: 'http://127.0.0.1:5000/login',
   SIGNUPINIT: 'https://has-server.herokuapp.com/signup/init',
   SIGNUPVERIFY: 'https://has-server.herokuapp.com/signup/verify',
-  LOGIN: 'http://127.0.0.1:5000/login',
+  LOGIN: 'https://has-server.herokuapp.com/login',
   GETJOBS: 'http://has-server.herokuapp.com/jobs/user/7',
   GETJOB: 'http://has-server.herokuapp.com/jobs/',
-  POSTJOB: 'http://127.0.0.1:5000/jobs/',
+  POSTJOB: 'https://has-server.herokuapp.com/jobs/',
   GETJOBBIDS: 'http://has-server.herokuapp.com/bids/job/',
   POSTBID: 'http://has-server.herokuapp.com/bids/',
-  GETSKILLS: 'http://127.0.0.1:5000/skills',
-  // GETJOBS: 'http://127.0.0.1:5000/jobs/user/7',
-  // GETJOB: 'http://127.0.0.1:5000/jobs/'
+  GETSKILLS: 'https://has-server.herokuapp.com/skills',
+  // GETJOBS: 'https://has-server.herokuapp.com/jobs/user/7',
+  // GETJOB: 'https://has-server.herokuapp.com/jobs/'
 };
 
 export const METHODS = {
@@ -21,10 +21,10 @@ export const METHODS = {
   SIGNUPVERIFY: 'POST'
 };
 
-export const baseUrl = 'http://127.0.0.1:5000';
+export const baseUrl = 'https://has-server.herokuapp.com';
 
 export const APIS = {
-  baseUrl: 'http://127.0.0.1:5000',
+  baseUrl: 'https://has-server.herokuapp.com',
   creditWallet: {
     path: '/wallet',
     method: 'POST'
